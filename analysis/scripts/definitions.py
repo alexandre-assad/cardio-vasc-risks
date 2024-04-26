18f09433ee4bcff539344a5eb2b16faebeb20b41
handriamaromanana <hugo.andriamaromanana@volta-medical.com>
2024-04-26 16:29:41 +0200

    [feat] now include pathology descriptions [fix] filter function now working [wip] classification for graphs

--- analysis/scripts/definitions.py
+++ analysis/scripts/definitions.py
@@ -3,9 +3,6 @@
 from enum import Enum, auto
 from typing import Dict
 
-from pydantic import BaseModel
-
-
 class CholesterolLevel(Enum):
     """Enum class for cholesterol levels"""
 
@@ -19,6 +16,7 @@ class GlucLevel(Enum):
     NORMAL = auto()
     ABOVE_NORMAL = auto()
     WELL_ABOVE_NORMAL = auto()
+
 
 class ApHighLevel(Enum):
     """Enum class for ap_hi levels"""
@@ -42,7 +40,7 @@ class ApLowLevel(Enum):
     INVALID = auto()
 
 
-_AP_HIGH_VALUES: Dict[ApHighLevel, int] = {
+AP_HIGH_VALUES: Dict[ApHighLevel, int] = {
     ApHighLevel.LOW: 90,
     ApHighLevel.NORMAL: 120,
     ApHighLevel.ELEVATED: 129,
@@ -50,7 +48,7 @@ _AP_HIGH_VALUES: Dict[ApHighLevel, int] = {
     ApHighLevel.HYPERTENSION_STAGE_2: 140,
 }
 
-_AP_LOW_VALUES: Dict[ApLowLevel, int] = {
+AP_LOW_VALUES: Dict[ApLowLevel, int] = {
     ApLowLevel.LOW: 60,
     ApLowLevel.NORMAL: 80,
     ApLowLevel.ELEVATED: 89,
@@ -58,81 +56,3 @@ _AP_LOW_VALUES: Dict[ApLowLevel, int] = {
     ApLowLevel.HYPERTENSION_STAGE_2: 100,
 }
 
-_ALLOWED_HEIGHT = range(60, 250)
-_ALLOWED_WEIGHT = range(30, 250)
-_ALLOWED_AGE = range(10, 110)
-
-
-class Patient(BaseModel):
-    """Patient model class"""
-
-    id: int
-    sex: bool
-    age: float
-    weight: float
-    height: float
-    ap_hi: float
-    ap_lo: float
-    cholesterol: float
-    gluc: float
-    smoke: bool
-    alco: bool
-    active: bool
-    cardio: bool
-
-    @property
-    def gender(self) -> str:
-        """Quick Repr of gender"""
-        return "Male" if not self.sex else "Female"
-
-    @property
-    def ap_hi_status(self) -> ApHighLevel:
-        """Method to get ap_hi status"""
-        ceilings = _AP_HIGH_VALUES.values()
-        accepted_range = range(min(ceilings), max(ceilings) + 1)
-        if self.ap_hi not in accepted_range:
-            return ApHighLevel.INVALID
-        for level, value in _AP_HIGH_VALUES.items():
-            if self.ap_hi <= value:
-                return level
-        raise ValueError("Invalid ap_hi value")
-
-    @property
-    def ap_lo_status(self) -> ApLowLevel:
-        """Method to get ap_lo status"""
-        floors = _AP_LOW_VALUES.values()
-        accepted_range = range(min(floors), max(floors) + 1)
-        if self.ap_lo not in accepted_range:
-            return ApLowLevel.INVALID
-        for level, value in _AP_LOW_VALUES.items():
-            if self.ap_lo <= value:
-                return level
-        raise ValueError("Invalid ap_lo value")
-
-    @property
-    def height_is_valid(self) -> bool:
-        """Method to get height status"""
-        return self.height in _ALLOWED_HEIGHT
-
-    @property
-    def weight_is_valid(self) -> bool:
-        """Method to get weight status"""
-        return self.weight in _ALLOWED_WEIGHT
-
-    @property
-    def age_is_valid(self) -> bool:
-        """Method to get age status"""
-        return self.age in _ALLOWED_AGE
-
-    @property
-    def is_valid(self) -> bool:
-        """Method to check if patient is valid"""
-        return all(
-            [
-                self.ap_hi_status != ApHighLevel.INVALID,
-                self.ap_lo_status != ApLowLevel.INVALID,
-                self.height_is_valid,
-                self.weight_is_valid,
-                self.age_is_valid,
-            ]
-        )
--- analysis/scripts/generic_methods.py
+++ analysis/scripts/generic_methods.py
@@ -1,31 +1,26 @@
 """Colletion of methods to convert and filter the dataset"""
+
 from pathlib import Path
 from typing import Callable
 
 from pandas import DataFrame, read_csv
 
-from scripts.definitions import Patient
-
-ID_STR = "id"
-AGE_STR = "age"
-HEIGHT_STR = "height"
-WEIGHT_STR = "weight"
-AP_LO_STR = "ap_lo"
-AP_HI_STR = "ap_hi"
-CHOLESTEROL_STR = "cholesterol"
-GENDER_STR = "gender"
-SMOKE_STR = "smoke"
-ALCO_STR = "alco"
-ACTIVE_STR = "active"
-CARDIO_STR = "cardio"
-GLUC_STR = "gluc"
+from analysis.scripts.create_patient import (
+    ACTIVE_STR,
+    ALCO_STR,
+    CARDIO_STR,
+    GENDER_STR,
+    SMOKE_STR,
+    create_patient,
+)
+from scripts.patient import Patient
 
 
 _CARDIO_DATASET_PATH = Path("../data/object_compatible/cardio_train.csv")
 CARDIO_DATASET = read_csv(_CARDIO_DATASET_PATH, sep=";")
 
 
-def booleanize_dataset(dataset: DataFrame) -> DataFrame:
+def _booleanize_dataset(dataset: DataFrame) -> DataFrame:
     """Method to convert dataset to boolean values"""
     dataset[GENDER_STR] = dataset[GENDER_STR].apply(lambda x: bool(x - 1))
     dataset[SMOKE_STR] = dataset[SMOKE_STR].apply(lambda x: bool(x))
@@ -34,37 +29,22 @@ def booleanize_dataset(dataset: DataFrame) -> DataFrame:
     dataset[CARDIO_STR] = dataset[CARDIO_STR].apply(lambda x: bool(x))
     return dataset
 
-def save_compatible_dataset(dataset: DataFrame) -> None:
+
+def _save_compatible_dataset(dataset: DataFrame) -> None:
     """Method to save dataset in a booleanized format"""
     comp_dataset_path = Path("../data/object_compatible/cardio_train.csv")
-    comp_dataset = booleanize_dataset(dataset)
+    comp_dataset = _booleanize_dataset(dataset)
     comp_dataset.to_csv(comp_dataset_path, sep=";", index=False)
 
 
-def drop_by_filter(
-    dataset: DataFrame, filter: Callable[[Patient], bool]
-) -> DataFrame:
+def drop_by_filter(dataset: DataFrame, filter: Callable[[Patient], bool]) -> DataFrame:
     """Method to filter dataset
     Pass a function that takes a Patient object and returns a boolean value
     All patients that return True will be removed from the dataset
     """
-    for index, patient_row in dataset.iterrows():
-        patient = Patient(
-            id=patient_row[ID_STR],
-            age=patient_row[AGE_STR],
-            height=patient_row[HEIGHT_STR],
-            weight=patient_row[WEIGHT_STR],
-            ap_lo=patient_row[AP_LO_STR],
-            ap_hi=patient_row[AP_HI_STR],
-            cholesterol=patient_row[CHOLESTEROL_STR],
-            gluc=patient_row[GLUC_STR],
-            smoke=patient_row[SMOKE_STR],
-            alco=patient_row[ALCO_STR],
-            active=patient_row[ACTIVE_STR],
-            cardio=patient_row[CARDIO_STR],
-            sex=patient_row[GENDER_STR],
-        )
+    new_dataset = dataset.copy()
+    for index, patient_row in new_dataset.iterrows():
+        patient = create_patient(patient_row)
         if filter(patient):
-            dataset = dataset.drop(index)
-    return dataset
-
+            new_dataset = new_dataset.drop(index)
+    return new_dataset
